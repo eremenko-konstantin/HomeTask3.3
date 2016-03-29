638f3581c643260af0bd43c536b3ca322d395716
Костя <eremenko.konstantin@gmail.com>
2016-03-29 11:30:54 +0300

    class GuessNumber

--- src/main/java/org/sourceit/GuessNumber.java
+++ src/main/java/org/sourceit/GuessNumber.java
@@ -1,7 +1,34 @@
 package org.sourceit;
 
+import java.util.Scanner;
+
 public class GuessNumber {
+
+
     public static void main(String[] args) {
-        System.out.println(5);
+
+        Scanner in = new Scanner(System.in);
+        int generateNumber = (int) (Math.random() * 100 + 1);
+        boolean win = false;
+        int number;
+
+        while(!win){
+            System.out.print("Введите число от 1 до 100 : ");
+            number = in.nextInt();
+            if(number < 1 || number > 100){
+            System.out.println("Вы ввели число не из заданного диапазона");
+            }
+            else{
+                if(number == generateNumber){
+                    win = true;
+                }else if(number < generateNumber){
+                    System.out.println("Слишком низко");
+                }else if(number > generateNumber){
+                    System.out.println("Слишком высоко");
+                }
+            }
+        }
+        System.out.println("Вы выграли");
+        System.out.println("Я загадал число " + generateNumber );
     }
 }
